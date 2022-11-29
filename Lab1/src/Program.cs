84964e0b168d3b0f701daeea35a4056962712c2f
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-11-29 03:20:22 +0200

    first

---
+++ Lab1/src/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+ 
+namespace Lab1
+{
+    class Program
+    {
+        public static string Shifrovka(string ishText, string pass,
+               string sol = "Kitty", string cryptographicAlgorithm = "SHA1",
+               int passIter = 2, string initVec = "a8doSuDitOz1hZe#",
+               int keySize = 256)
+        { 
+            byte[] initVecB = Encoding.ASCII.GetBytes(initVec);
+            byte[] solB = Encoding.ASCII.GetBytes(sol);
+            byte[] ishTextB = Encoding.UTF8.GetBytes(ishText);
+ 
+            PasswordDeriveBytes derivPass = new PasswordDeriveBytes(pass, solB, cryptographicAlgorithm, passIter);
+            byte[] keyBytes = derivPass.GetBytes(keySize / 8);
+            RijndaelManaged symmK = new RijndaelManaged();
+            symmK.Mode = CipherMode.CBC;
+ 
+            byte[] cipherTextBytes = null;
+ 
+            using (ICryptoTransform encryptor = symmK.CreateEncryptor(keyBytes, initVecB))
+            {
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(ishTextB, 0, ishTextB.Length);
+                        cryptoStream.FlushFinalBlock();
+                        cipherTextBytes = memStream.ToArray();
+                        memStream.Close();
+                        cryptoStream.Close();
+                    }
+                }
+            }
+ 
+            symmK.Clear();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+ 
+        
+        static void Main(string[] args)
+        {
+ 
+            String gg = Shifrovka("Shapka, 922v", "пароль");            
+            Console.WriteLine(gg);
+            Console.WriteLine("Shapka, 922v");
+        }
+    }
+}
